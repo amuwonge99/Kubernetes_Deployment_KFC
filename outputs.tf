71b856d04f519a5bbaddc6b475c73691b8fda6ea
Gus M <amuwonge99@gmail.com>
2025-09-01 12:06:19 +0100

    applyed terraform

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,12 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
 }
 
 output "region" {
