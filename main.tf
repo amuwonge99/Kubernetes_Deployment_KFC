258d45138783d4eb572591c422b25d0fcf46c83b
Gus M <amuwonge99@gmail.com>
2025-09-03 21:05:38 +0100

    removed s3 resource, and its backend

--- main.tf
+++ main.tf
@@ -6,12 +6,6 @@
 
 terraform { 
 
-    backend "s3" {
-    bucket         = "kfc-bucket-for-mo-to-enjoy"
-    key            = "eks/terraform.tfstate"
-    region         = "eu-west-2"
-   }
-  
   required_version = ">= 1.3"
   required_providers {
     aws = {
